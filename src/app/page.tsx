7de10eee830402ce52d8e60d4cc297d5745598fa
HIMANSHU SHUKLA <120308745+raghavshulka@users.noreply.github.com>
2024-07-28 06:46:43 +0530

    made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,37 @@
 import React from "react";
+import Image from "next/image";
 import bb from "../../public/bb.jpg";
 import Rendermodel from "@/Components/Rendermodel";
-import Image from "next/image";
 import Header from "@/Components/Header";
 import RenderButterfly from "@/Components/RenderButterfly";
 
 const Home: React.FC = () => {
   return (
     <div className="scroll-smooth">
-      <div className="relative w-full h-screen ">
+      {/* First Section: Background Image and 3D Model */}
+      <section className="relative w-full h-screen">
         <Image
           src={bb}
           alt="background"
-          fill
-          style={{ objectFit: "cover" }}
+          layout="fill"
+          objectFit="cover"
           className="blur-md"
         />
-        <Rendermodel />
+        <div className="absolute inset-0">
+          <Rendermodel />
+        </div>
+      </section>
 
-      </div>
-
-      <div className=" w-full h-screen bg-black flex md:flex-row flex-col justify-center items-center " >
-        <Header/>
-
-        <RenderButterfly/>
-      </div>
-      </div>
+      {/* Second Section: Header and Butterfly */}
+      <section className="w-full h-screen bg-black flex flex-col md:flex-row justify-center items-center">
+        <div className="w-full md:w-1/2 p-4">
+          <Header />
+        </div>
+        <div className="w-full md:w-1/2 h-full">
+          <RenderButterfly />
+        </div>
+      </section>
+    </div>
   );
 };
 
